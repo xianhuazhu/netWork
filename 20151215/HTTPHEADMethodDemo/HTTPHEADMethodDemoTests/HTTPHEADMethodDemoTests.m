e6ff30014d5b7f4b6482d6216cfe6cec5c9216dd
xianhua <13674945081@sina.com>
2015-12-22 17:58:40 +0800

    下载音乐

---
+++ 20151215/HTTPHEADMethodDemo/HTTPHEADMethodDemoTests/HTTPHEADMethodDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  HTTPHEADMethodDemoTests.m
+//  HTTPHEADMethodDemoTests
+//
+//  Created by qingyun on 15/12/17.
+//  Copyright © 2015年 河南青云信息技术有限公司. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HTTPHEADMethodDemoTests : XCTestCase
+
+@end
+
+@implementation HTTPHEADMethodDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
